666f0d6d443a83933930ed467127ad8cc9b71b2c
isszero06 <76986925+isszero06@users.noreply.github.com>
2022-08-06 10:57:39 +0200

    Add files via upload

---
+++ so_edit_date_order/models/sale.py
@@ -0,0 +1,39 @@
+
+from operator import mod
+from odoo import fields, models, _, api
+from datetime import date, datetime
+from odoo.exceptions import AccessError, UserError, ValidationError
+
+
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    _description = "Sales Order Edit order date"
+
+
+    def action_confirm(self):
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
+
+        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+            order.message_subscribe([order.partner_id.id])
+        if str(self.date_order.date()) < str(date.today()):
+            self.write({
+                'state': 'sale',
+            })
+        else:
+            self.write({
+                'state': 'sale',
+                'date_order': fields.Datetime.now()
+            })
+
+        context = self._context.copy()
+        context.pop('default_name', None)
+
+        self.with_context(context)._action_confirm()
+        if self.env.user.has_group('sale.group_auto_done_setting'):
+            self.action_done()
+        return True
