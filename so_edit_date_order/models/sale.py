e14de42e1534b00b68f0dcbf205d13aafe89a378
isszero06 <76986925+isszero06@users.noreply.github.com>
2022-08-07 08:38:14 +0200

    Update sale.py

--- so_edit_date_order/models/sale.py
+++ so_edit_date_order/models/sale.py
@@ -1,10 +1,19 @@
 
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Zero For Information Systems (<www.erpzero.com>)
+# Copyright(c): 2016-Zero For Information Systems
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#################################################################################
+
 from operator import mod
 from odoo import fields, models, _, api
 from datetime import date, datetime
 from odoo.exceptions import AccessError, UserError, ValidationError
-
-
 
 
 class SaleOrder(models.Model):
