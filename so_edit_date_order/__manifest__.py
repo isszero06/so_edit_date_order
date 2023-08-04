267531d43e612db578b1e2d7a404d9808706ae7d
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-08-04 20:55:22 +0300

    Update __manifest__.py

--- so_edit_date_order/__manifest__.py
+++ so_edit_date_order/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Sales Order Date Order Visible and Editable',
-    'version': '15.0.1',
+    'version': '15.0.2',
     'category': 'Sales',
     "author": 'Zero Systems',
     "company": 'Zero for Information Systems',
