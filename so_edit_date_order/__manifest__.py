939ca05ce59859f347bc012c9c526a812ff7a0e9
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-11-25 12:02:46 +0200

    Update __manifest__.py

--- so_edit_date_order/__manifest__.py
+++ so_edit_date_order/__manifest__.py
@@ -35,7 +35,7 @@
     'installable': True,
     'auto_install': False,
     'application': False,
-    'price': 15.0,
+    'price': 00.0,
     'currency': 'EUR',
     "pre_init_hook": "pre_init_check",
 }
