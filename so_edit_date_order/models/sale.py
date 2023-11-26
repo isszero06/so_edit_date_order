f8095c7aaa153d33d828fa2fa4ab17f9ccbeed25
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-11-26 11:17:07 +0200

    Update sale.py

--- so_edit_date_order/models/sale.py
+++ so_edit_date_order/models/sale.py
@@ -20,28 +20,13 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
-    def action_confirm(self):
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
-
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
+    def _prepare_confirmation_values(self):
         if str(self.date_order.date()) < str(date.today()):
-            self.write({
+            return {
                 'state': 'sale',
-            })
+            }
         else:
-            self.write({
+            return {
                 'state': 'sale',
                 'date_order': fields.Datetime.now()
-            })
-
-        context = self._context.copy()
-        context.pop('default_name', None)
-
-        self.with_context(context)._action_confirm()
-        if self.env.user.has_group('sale.group_auto_done_setting'):
-            self.action_done()
-        return True
+            }
